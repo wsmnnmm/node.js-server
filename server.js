05119b231b12cf3387ab0a205f622dd4b560f9a7
wsmnnmm <1832004940@qq.com>
2021-12-02 19:44:37 +0800

    最后一次

--- server.js
+++ server.js
@@ -12,8 +12,23 @@ if (!port) {
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'content-type': 'text/html' })
     fs.createReadStream('index.html').pipe(res)
-    fs.createReadStream('1.jpg').pipe(res)
+    if (req.url == "/index.html") {
+        res.end(index.html)
+        return;
+    }
+
+    //to display image
+    if (req.url == "/1.jpg") {
+
+        var img = fs.readFileSync('./1.jpg');
+        res.writeHead(200, { 'Content-Type': 'image/jpg' });
+        res.end(img, 'binary');
+
+        return;
+
+    }
 })
+
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
