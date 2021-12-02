b6b94b1ed6f5fca83122f7797f941daa8887e89c
wsmnnmm <1832004940@qq.com>
2021-12-02 20:18:35 +0800

    最初状态

--- server.js
+++ server.js
@@ -2,33 +2,58 @@ var http = require('http')
 var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
-var index = fs.readFileSync('index.html');
 
 if (!port) {
     console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
     process.exit(1)
 }
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    fs.createReadStream('index.html').pipe(res)
-    if (req.url == "/index.html") {
-        res.end(index.html)
-        return;
+var server = http.createServer(function (request, response) {
+    var parsedUrl = url.parse(request.url, true)
+    var pathWithQuery = request.url
+    var queryString = ''
+    if (pathWithQuery.indexOf('?') >= 0) { queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+    var path = parsedUrl.pathname
+    var query = parsedUrl.query
+    var method = request.method
+
+    /******** 从这里开始看，上面不要看 ************/
+
+    console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+
+    if (path === '/') {
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.write(`
+            <!DOCTYPE html>
+            <head>
+                <link rel="stylesheet" href="/x">
+            </head>
+            <body>
+                <h1>wsmnnmm</h1>
+                <script src="/y"> </script>
+            </body>
+        `)
+        response.end()
+    } else if (path === '/x') {
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/css;charset=utf-8')
+        response.write(`body{color: red;}`)
+        response.end()
+    } else if (path === "/y") {
+        response.setHeader("Content-Type", "text/javascript;charset=utf-8");
+        response.write(`console.log('这是js内容')`);
+        response.end();
+    }
+    else {
+        response.statusCode = 404
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.write(`你访问的页面不存在,404`)
+        response.end()
     }
 
-    //to display image
-    if (req.url == "/1.jpg") {
-
-        var img = fs.readFileSync('./1.jpg');
-        res.writeHead(200, { 'Content-Type': 'image/jpg' });
-        res.end(img, 'binary');
-
-        return;
-
-    }
+    /******** 代码结束，下面不要看 ************/
 })
-
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
