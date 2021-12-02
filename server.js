041c2d7833324220ae99e35e4cfcf8e085c3469a
wsmnnmm <1832004940@qq.com>
2021-12-03 00:39:18 +0800

    改了bug

--- server.js
+++ server.js
@@ -1,25 +1,8 @@
-var http = require('http')
-var fs = require('fs')
-var url = require('url')
-var port = process.argv[2]
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
-if (!port) {
-    console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
-    process.exit(1)
-}
-
-var server = http.createServer(function (request, response) {
-    var parsedUrl = url.parse(request.url, true)
-    var pathWithQuery = request.url
-    var queryString = ''
-    if (pathWithQuery.indexOf('?') >= 0) { queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
-    var path = parsedUrl.pathname
-    var query = parsedUrl.query
-    var method = request.method
-
-    /******** 从这里开始看，上面不要看 ************/
-
-    console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+http.createServer(function (req, res) {
 
     if (req.url === "/") {
         fs.readFile("./public/index.html", "UTF-8", function (err, html) {
@@ -42,9 +25,4 @@ var server = http.createServer(function (request, response) {
         res.end("No Page Found");
     }
 
-    /******** 代码结束，下面不要看 ************/
-})
-
-server.listen(port)
-console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
-
+}).listen(3000);
