27838fa46c400396570eee48d1fb2330cfa1c27a
wsmnnmm <1832004940@qq.com>
2021-12-02 18:25:00 +0800

    修改了路径

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ var server = http.createServer(function (request, response) {
         <body>
             <h1>wsmnnmm</h1>
             <script src="/y"> </script>
-            <img src=`+ img + ` />
+            <img src="`+ img + `" />
         </body>
         
         </html>`)
