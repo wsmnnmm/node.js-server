3267d7d9ba215ba818da52205c24d85dd5d54ec9
wsmnnmm <1832004940@qq.com>
2021-12-02 18:14:00 +0800

    改了

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var server = http.createServer(function (request, response) {
     if (path === '/') {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.writeFile(`<!DOCTYPE html>
+        response.write(`<!DOCTYPE html>
         <html lang="en">
         
         <head>
