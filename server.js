6238ccb74be244ce8c4ce70adec92b240ea76fd1
wsmnnmm <1832004940@qq.com>
2021-12-02 19:17:25 +0800

    修改成文件

--- server.js
+++ server.js
@@ -2,67 +2,16 @@ var http = require('http')
 var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
-var img = 'https://github.com/wsmnnmm/node.js-server/blob/b11c51c31e8b08ba9098cb19e00fbc8bdf958ad2/1.jpg'
+var index = fs.readFileSync('index.html');
 
 if (!port) {
     console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
     process.exit(1)
 }
 
-var server = http.createServer(function (request, response) {
-    var parsedUrl = url.parse(request.url, true)
-    var pathWithQuery = request.url
-    var queryString = ''
-    if (pathWithQuery.indexOf('?') >= 0) { queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
-    var path = parsedUrl.pathname
-    var query = parsedUrl.query
-    var method = request.method
-
-    /******** 从这里开始看，上面不要看 ************/
-
-    console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
-
-    if (path === '/') {
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`<!DOCTYPE html>
-        <html lang="en">
-        
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-            <link rel="stylesheet" href="/x">
-        </head>
-        
-        <body>
-            <h1>wsmnnmm</h1>
-            <script src="/y"> </script>
-            <img src="`+ img + `" />
-            <a href="`+ img + `">帅哥</a>
-        </body>
-        
-        </html>`)
-        response.end()
-    } else if (path === '/x') {
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'text/css;charset=utf-8')
-        response.write(`body{color: red;}`)
-        response.end()
-    } else if (path === "/y") {
-        response.setHeader("Content-Type", "text/javascript;charset=utf-8");
-        response.write(`console.log('这是js内容')`);
-        response.end();
-    }
-    else {
-        response.statusCode = 404
-        response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`你访问的页面不存在,404`)
-        response.end()
-    }
-
-    /******** 代码结束，下面不要看 ************/
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    fs.createReadStream('index.html').pipe(res)
 })
 
 server.listen(port)
